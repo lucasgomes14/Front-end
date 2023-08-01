efba25d018218ddde24b2061bcdb75085467391a
Lucas Matheus <lucazmatehus14@gmail.com>
2023-08-01 15:20:50 -0300

    Começando o capítulo de Node.js

---
+++ node/pastaA/pastaB/acessarModulos-152.js
@@ -0,0 +1,11 @@
+const moduloA = require("../../moduloA-150");
+console.log(moduloA.ola);
+
+const c = require("./pastaC");
+console.log(c.ola2);
+
+/*const http = require("http");
+http.createServer((req, res) => {
+	res.write("Bom dia!");
+	res.end();
+}).listen(8080);*/
