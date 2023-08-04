b3bf43c4fd56211dd377635dbeee60e6ef7c556f
Lucas Matheus <lucazmatehus14@gmail.com>
2023-08-04 11:58:53 -0300

    Continuando o projeto interagindo com o banco

--- node/projeto/src/servidor.js
+++ node/projeto/src/servidor.js
@@ -2,11 +2,25 @@ const porta = 3003;
 
 const express = require("express");
 const app = express();
+const bancoDeDados = require("./bancoDeDados");
 
 app.get("/produtos", (req, res, next) => {
-	res.send({ nome: "Notebook", preco: 123.45 }) // Converter para JSON
+	res.send(bancoDeDados.getProdutos()); // Converter para JSON
 });
+
+app.get("/produtos/:id", (req, res, next) => {
+	res.send(bancoDeDados.getProduto(req.params.id));
+});
+
+app.post("/produtos", (req, res, next) => {
+	const produto = bancoDeDados.salvarProdutos({
+		nome: req.body.nome,
+		preco: req.body.preco
+	});
+
+	res.send(produto); // JSON
+})
 
 app.listen(porta, () => {
 	console.log(`Servidor executando na porta ${porta}.`)
-})
+});
